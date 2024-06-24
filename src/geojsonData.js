90ae8a1670b025713760e1d906c4c04fde2ccbc3
Sana Abdullah <sana200229@gmail.com>
2024-06-24 12:19:09 +0300

    Map with coloured regions

---
+++ src/geojsonData.js
@@ -0,0 +1,25 @@
+export const data = 
+{
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "id": "1",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [
+            [
+              [10.0, 35.0],
+              [20.0, 35.0],
+              [20.0, 45.0],
+              [10.0, 45.0],
+              [10.0, 35.0]
+            ]
+          ]
+        },
+        "properties": {}
+      }
+    ]
+  }
+
+  
